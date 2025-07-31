753b594685afadacecdfcb4cbffcad55b2f5516a
Nguyễn Văn Ba <nguyenvanbapc66@gmail.com>
2025-07-31 11:15:36 +0700

    feat: complete task 1

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,14 @@ function App() {
             <main className="flex-grow">
               <Routes>
                 {/* Public Routes */}
-                <Route path="/" element={<Landing />} />
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute requiredRole="user">
+                      <Landing />
+                    </ProtectedRoute>
+                  }
+                />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
